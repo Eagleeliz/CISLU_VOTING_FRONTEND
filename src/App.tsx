b1df9886658e2b2fd04c1250ba2a271dc7ab0a1f
Eagleeliz <lizwanjiku573@gmail.com>
2026-02-10 23:53:12 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+  
+    
+      <div className=" text-red-900  bg-pink-200">
+          Golden Home Hotels
+        </div>
+  
+  )
+}
+
+export default App
